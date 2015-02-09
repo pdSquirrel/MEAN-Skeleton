d94891f3c798f5159c26e15f4a999bb028a43004
Roger Nordqvist <progdev.squirrel@gmail.com>
2015-02-09 22:32:35 +0100

    Added view engine Jade

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var app = express();
+
+app.set('views', __dirname + '/server/views');
+app.set('view engine', 'jade');
+
+app.get('*', function(req, res){
+  res.render('index');
+});
 
 app.listen(3030, function(){
   console.log("Server started on http://localhost:3030");
