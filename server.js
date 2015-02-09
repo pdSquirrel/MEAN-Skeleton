7322e1245324f74e3704643acc2b2bd2f1b0f2d5
Roger Nordqvist <progdev.squirrel@gmail.com>
2015-02-09 22:50:09 +0100

    Added morgan, body-parser and stylus

--- server.js
+++ server.js
@@ -1,16 +1,41 @@
-var express = require('express');
+var express = require('express'),
+    stylus  = require('stylus'),
+    logger  = require('morgan'),
+    bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3030;
 
 var app = express();
 
+function compile(str, path){
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+
+/* render line breaks from jade templates when in dev */
+if (app.get('env') === 'development') {
+  app.locals.pretty = true;
+}
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+    {
+      src: __dirname + '/public',
+      compile: compile
+    }
+));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res){
   res.render('index');
 });
 
-app.listen(3030, function(){
+app.listen(port, function(){
   console.log("Server started on http://localhost:3030");
 });
